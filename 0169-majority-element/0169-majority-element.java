501a32ecb720857bbd13b09bdad2d403c15b9b5f
viswanath999 <bgmmusic62@gmail.com>
2026-01-23 19:54:39 +0530

    Time: 1 ms (99.82%), Space: 55.6 MB (53.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
         int leader=nums[0];
-        int majority=1;
+        int majority=0;
         for(int i=0;i<nums.length;i++){
             if(majority==0){
                 leader=nums[i];
